cb29344983e8bb743d3e9e220207bf8da30e5028
Pranoy R <pranoyalkr@gmail.com>
2020-07-11 15:28:14 +0530

    size changed to 224

--- trainer.py
+++ trainer.py
@@ -161,7 +161,7 @@ device = torch.device(f"cuda:{opt.gpu}" if opt.use_cuda else "cpu")
 # 					]))
 
 transform = transforms.Compose([
-						   transforms.Resize((150,150)),
+						   transforms.Resize((224,224)),
 						   transforms.ToTensor(),
 						   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
 					0.229, 0.224, 0.225])

5379a293b43cf379930ccb9ae4b64e26ab8b3dc2
Pranoy R <pranoyr@Pranoy-2.local>
2020-12-21 21:54:47 +0530

    updated

--- trainer.py
+++ trainer.py
@@ -232,8 +232,8 @@ if (__name__ == '__main__'):
 	np.random.shuffle(indices)
 	train_idx, valid_idx = indices[:split], indices[split:]
 
-	train_idx = [i for i in range (100)]
-	valid_idx = [i for i in range (100)]
+	train_idx = [i for i in range (1000)]
+	valid_idx = [i for i in range (1000, 1500)]
 
 	train_dataset = Subset(train_dataset, indices=train_idx)
 	val_dataset = Subset(val_dataset, indices=valid_idx)

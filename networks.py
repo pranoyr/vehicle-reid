a1d53c127fd28f699459d5a0fee3827ab2e49692
Pranoy R <pranoyr@Pranoy-2.local>
2021-01-13 13:28:12 +0530

    cosine loss added

--- networks.py
+++ networks.py
@@ -1,10 +1,53 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet18
-from resnet import resnet10
+# from resnet import resnet10
 from torchvision.models import mobilenet_v2
 import torch
 
+
+class BasicBlock(nn.Module):
+	def __init__(self, c_in, c_out,is_downsample=False):
+		super(BasicBlock,self).__init__()
+		self.is_downsample = is_downsample
+		if is_downsample:
+			self.conv1 = nn.Conv2d(c_in, c_out, 3, stride=2, padding=1, bias=False)
+		else:
+			self.conv1 = nn.Conv2d(c_in, c_out, 3, stride=1, padding=1, bias=False)
+		self.bn1 = nn.BatchNorm2d(c_out)
+		self.relu = nn.ReLU(True)
+		self.conv2 = nn.Conv2d(c_out,c_out,3,stride=1,padding=1, bias=False)
+		self.bn2 = nn.BatchNorm2d(c_out)
+		if is_downsample:
+			self.downsample = nn.Sequential(
+				nn.Conv2d(c_in, c_out, 1, stride=2, bias=False),
+				nn.BatchNorm2d(c_out)
+			)
+		elif c_in != c_out:
+			self.downsample = nn.Sequential(
+				nn.Conv2d(c_in, c_out, 1, stride=1, bias=False),
+				nn.BatchNorm2d(c_out)
+			)
+			self.is_downsample = True
+
+	def forward(self,x):
+		y = self.conv1(x)
+		y = self.bn1(y)
+		y = self.relu(y)
+		y = self.conv2(y)
+		y = self.bn2(y)
+		if self.is_downsample:
+			x = self.downsample(x)
+		return F.relu(x.add(y),True)
+
+def make_layers(c_in,c_out,repeat_times, is_downsample=False):
+	blocks = []
+	for i in range(repeat_times):
+		if i ==0:
+			blocks += [BasicBlock(c_in,c_out, is_downsample=is_downsample),]
+		else:
+			blocks += [BasicBlock(c_out,c_out),]
+	return nn.Sequential(*blocks)
 
 
 class Net(nn.Module):

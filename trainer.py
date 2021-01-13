d422bb4a5f913fdbd0d40f6e4e035c6ef4fa13ca
Pranoy R <pranoyr@Pranoy-2.local>
2021-01-13 13:27:28 +0530

    cosine loss added

--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ import tensorboardX
 
 from losses import TripletLoss
 from datasets import TripletMNIST, TripletVeriDataset
-from networks import TripletNet, EmbeddingNet, Resnet18, MobileNetv2
+from networks import TripletNet, EmbeddingNet, MobileNetv2, Net
 from metrics import AccumulatedAccuracyMetric
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 from torchvision import datasets, transforms
@@ -213,7 +213,7 @@ if (__name__ == '__main__'):
 	val_loader = torch.utils.data.DataLoader(validation_data,
 											 batch_size=opt.batch_size, shuffle=True, num_workers=opt.num_workers)
 
-	embedding_net = Resnet18()
+	embedding_net = Net()
 	# embedding_net = MobileNetv2()
 	model = TripletNet(embedding_net).to(device)
 	# loss_fn = nn.TripletMarginLoss(margin=0.5)
